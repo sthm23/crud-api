b189e49acc4a90a75dc6c1401dc97687fc1fd1b7
Tukhtamishev Sanjar <stuxtamixhev@gmail.com>
2022-06-18 14:23:33 +0500

    fix: check ID function

--- src/controllers/controllers.js
+++ src/controllers/controllers.js
@@ -16,6 +16,9 @@ let users = [
 ];
 
 function checkId(id){
+  if(id.length !== 31){
+    return false;
+  }
   const hex = ['a','b','c','d','e','f','A','B','C','D','E','F','0','1','2','3','4','5','6','7','8','9','-'];
   for(let i=0; i<id.length; i++){
     if(!hex.includes(id[i])){
