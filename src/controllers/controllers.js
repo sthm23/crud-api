7b0497e6c767eed267dbfe562597ea59094d2143
Tukhtamishev Sanjar <stuxtamixhev@gmail.com>
2022-06-18 12:48:15 +0500

    feat: done create user

--- src/controllers/controllers.js
+++ src/controllers/controllers.js
@@ -22,7 +22,8 @@ function getUsers(res, req){
 }
 
 function getUserById(res, req, id){
-  const user = users.find(item=>item.id === id);
+  const user = users.find(item=>item.id == id);
+
   if(user){
     res.writeHead(200, { 'Content-Type': 'application/json' });
     res.end(JSON.stringify(user));
@@ -32,4 +33,31 @@ function getUserById(res, req, id){
   }
 }
 
-export {getUsers, getUserById}
+async function createUser(res, req){
+  let str = '';
+
+  req.on('data', chunk=>{
+    str+=chunk.toString();
+  });
+
+  req.on('end', async()=>{
+    const data = await JSON.parse(str);
+
+    const user = data.hasOwnProperty("username");
+    const ages = data.hasOwnProperty("age");
+    const hobbie = data.hasOwnProperty("hobbies") && Array.isArray(data.hobbies);
+
+    if(user && ages && hobbie){
+      const newUser = {id: v4(), ... data};
+      users.push(newUser);
+      res.writeHead(200, { 'Content-Type': 'application/json' });
+      res.end(JSON.stringify(newUser));
+    }else{
+      res.writeHead(400, { 'Content-Type': 'application/json' });
+      res.end(JSON.stringify({message: "all prop don't write"}));
+    }
+  });
+  
+}
+
+export {getUsers, getUserById, createUser}
--- src/index.mjs
+++ src/index.mjs
@@ -1,14 +1,17 @@
 import { createServer } from "http";
-import {  getUsers, getUserById} from './controllers/controllers.js';
+import {  getUsers, getUserById, createUser} from './controllers/controllers.js';
 
 const PORT = process.env.PORT || 3000;
 const server = createServer((req, res)=>{
+ 
 
   if(req.url === '/api/users' && req.method === 'GET'){
     getUsers(res, req);
-  }else if(req.url.match(/\/api\/products\/([0-9]+)/) && req.method === 'GET'){
+  }else if(req.url.match(/\/api\/users\/([0-9]+)/) && req.method === 'GET'){
     const id = req.url.split('/')[3];
     getUserById(res, req, id);
+  }else if(req.url === `/api/users` && req.method === 'POST'){
+    createUser(res, req);
   }else{
     res.writeHead(400, { 'Content-Type': 'application/json' });
     res.end(JSON.stringify({"Message": "Error"}));
